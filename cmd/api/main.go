fbf49be9203f93572842bc08e366b14d51bee148
Israel Parodi <israelps97@gmail.com>
2023-10-29 21:42:49 -0500

    Added mailer and graceful shutdown on goroutines

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,12 +5,14 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
 	"golang.org/x/exp/slog"
 	configuration "gorest.israel.parodi/config"
 	"gorest.israel.parodi/internal/data"
+	"gorest.israel.parodi/internal/mailer"
 )
 
 const version = "1.0.0"
@@ -29,12 +31,21 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *slog.Logger
 	models data.Models
+	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 var env configuration.Environment
@@ -61,6 +72,12 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "1cb0574946ef90", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "bf10e7b36334d3", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Go Rest <no-reply@gorest.iparodi.net>", "SMTP sender")
+
 	flag.Parse()
 
 	db, err := openDB(cfg)
@@ -77,6 +94,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
